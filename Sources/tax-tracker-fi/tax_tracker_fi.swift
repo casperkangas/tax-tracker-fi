c7f169a117d609defadb0a83c6bec1113bf2e997
casperkangas <casperkangas@gmail.com>
2026-06-29 23:12:31 +0300

    Add initial package structure and main executable

---
+++ Sources/tax-tracker-fi/tax_tracker_fi.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct tax_tracker_fi {
+    static func main() {
+        print("Hello, world!")
+    }
+}
---
+++ Tests/tax-tracker-fiTests/tax_tracker_fiTests.swift
@@ -0,0 +1,8 @@
+import Testing
+@testable import tax_tracker_fi
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    // Swift Testing Documentation
+    // https://developer.apple.com/documentation/testing
+}
